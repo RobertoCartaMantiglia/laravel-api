8f866d38318787ab1404bf12d673d30e9f7de380
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-03-02 16:36:31 +0100

    api model and controller and api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProjectController as ApiProjectController;
+use Doctrine\DBAL\Schema\Index;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/projects', [ApiProjectController::class, 'index'])->name('api.projects.index');
