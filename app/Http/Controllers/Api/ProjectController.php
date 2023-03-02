fce57dd2a7ff3ed20acb804b9b2414e4a21c5208
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-03-02 16:46:17 +0100

    create json

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,15 +4,18 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
+
 
 class ProjectController extends Controller
 
 {
     public function index()
     {
+        $projects = Project::paginate(5);
         return response()->json([
-            'nome' => "Roberto",
-            'cognome' => "cartmanz"
+            'success' => true,
+            'result' => $projects,
         ]);
     }
 }
